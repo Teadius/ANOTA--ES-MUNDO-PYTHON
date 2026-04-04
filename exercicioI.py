a9b4297f6a5dc7d67fbfbd5c3ddea69a694ec97b
Seu Nome <seuemail@gmail.com>
2026-04-04 16:18:07 -0300

    v0.1

---
+++ exercicioI.py
@@ -0,0 +1,11 @@
+nome = input("digite seu nome: ")
+if nome == "jonathan":
+    print(f"bom dia {nome}")
+else:
+    print(f"bom dia seu nome é {nome}")
+
+nacimentodia = input("digite seu dia de nascimento: ")
+nacimentomes = input("digite seu mes de nascimento: ")
+nacimentoano = input("digite seu ano de nascimento: ")
+
+print(f"você nasceu no dia {nacimentodia} do mês {nacimentomes} do ano {nacimentoano}")
