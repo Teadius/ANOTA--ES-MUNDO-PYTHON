09de4aa2b21474bad58c97066e35753a2637c2cd
Seu Nome <seuemail@gmail.com>
2026-04-04 16:24:25 -0300

    troca de exercicioI por exercicio1

--- exercicio1.py
+++ exercicio1.py
@@ -9,3 +9,8 @@ nacimentomes = input("digite seu mes de nascimento: ")
 nacimentoano = input("digite seu ano de nascimento: ")
 
 print(f"você nasceu no dia {nacimentodia} do mês {nacimentomes} do ano {nacimentoano}")
+
+num1 = input("digite um numero: ")
+num2 = input("digite outro numero: ")
+
+print(f'a soma de num1 e num2 é: {int(num1) + int(num2)}')
