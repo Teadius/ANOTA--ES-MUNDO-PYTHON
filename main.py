3f8aef7b5ce2a1b9b6d2db13c0d9061237b630bc
Seu Nome <seuemail@gmail.com>
2026-04-04 16:05:37 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+
+# print("Hello World"), ou seja escrever hello world na tela é uma tradição
+# para dar sorte no apendizado de uma nova linguagem
+print("Hello World")
+
+
+
+# Para que serve o Python? historia do python e mais...
+
+#O python foi criado por Guido van Rossum e lançado em 1991. 
+#   Ele é uma linguagem de programação de alto nível, interpretada e de 
+# propósito geral. O Python é conhecido por sua sintaxe clara e legível, o 
+# que o torna uma ótima escolha para iniciantes em programação. 
+#   Ele suporta múltiplos paradigmas de programação, incluindo programação 
+# orientada a objetos, programação funcional e programação imperativa. 
+#   O Python é amplamente utilizado em diversas áreas, como desenvolvimento
+# web, ciência de dados, inteligência artificial, automação e muito mais.
+#  O nome python vem do programa de TV "Monty Phython's Fliying Circus"
+#  A cobra foi escolhida como mascote do Python por causa do nome, e não 
+# por causa do livro da editora O'Reilly, que tem uma cobra na capa, a 
+# partir deste livro a cobra foi aceita com principal simbolo.
+# Em 1995 a CP4E computer programming for everybody, foi criado para 
+# ensinar programação para pessoas sem experiência prévia, financiado pela 
+# DARPA. 
+# A partir de 2001 o python passou a evoluir com a comunidade pelo python 
+# softwere foundation, que é uma organização sem fins lucrativos dedicada 
+# a promover o desenvolvimento e o uso do Python.
+
+# O python e uma linguagem multiplataforma facil intuitivo e pode fazer 
+# basicamente tudo(mas não se especializa em tudo).
+
+#import this
+# zen of python 
+# 1. Bonito é melhor que feio.
+# 2. Explícito é melhor que implícito.
+# 3. Simples é melhor que complexo.
+# 4. Complexo é melhor que complicado.
+# 5. Linear é melhor que aninhado.
+# 6. Esparso é melhor que denso.
+# 7. Legibilidade conta.
+# 8. Casos especiais não são especiais o suficiente para quebrar as regras.
+# 9. Embora a praticidade vença a pureza.
+# 10. Erros nunca devem passar silenciosamente.
+# 11. A menos que sejam explicitamente silenciados.
+# 12. Diante de ambiguidade, recuse a tentação de adivinhar.
+# 13. Deve haver uma - e preferencialmente apenas uma - maneira óbvia de fazer algo.
+# 14. Embora essa maneira possa não ser óbvia à primeira vista, a menos que você seja holandês.
+# 15. Agora é melhor que nunca.
+# 16. Embora nunca seja frequentiemnte seja melhor do que *já*
+# 17. Se a implementação é difícil de explicar, é uma má ideia.
+# 18. Se a implementação é fácil de explicar, pode ser uma boa ideia.
+# 19. Namespaces são uma ótima ideia - vamos fazer mais dessas!
+
+
+
+# O computador le linguagem de maquina (0 e 1) o programador se comunica com
+# um interpretador ou compilador, como o python, que traduz o código para 
+# linguagem de maquina, o python é uma linguagem interpretada, ou seja, o 
+# código é traduzido linha por linha, o que facilita a depuração e o 
+# desenvolvimento, mas pode ser mais lento do que uma linguagem compilada, 
+# como o C ou o C++.
+
+linguagem01 = "Hello"
+print(linguagem01)
+
+#print escreve input le
+
+nome = input("Qual é o seu nome? ")
+idade = input("Qual é a sua idade? ")
+altura = input("Qual é a sua altura? ")
+
+print(f'seu nome e {nome}, sua idade é {idade} e sua altura é {altura}')
