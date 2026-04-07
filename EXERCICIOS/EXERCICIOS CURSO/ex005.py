26f92143d01e6a47ccb8fafe1626c9c620e71856
Seu Nome <seuemail@gmail.com>
2026-04-07 16:12:02 -0300

    sav001

--- EXERCICIOS/EXERCICIOS CURSO/ex005.py
+++ EXERCICIOS/EXERCICIOS CURSO/ex005.py
@@ -1,11 +1,4 @@
 numero = float(input('digite um numero: '))
+
 print('o numero digitado foi {}\n seu antecessor é {} \n e seu sucessor e {}.  '.format(numero, numero-1, numero+1))
 print('{} \n {} \n {}'.format(numero-1, numero, numero+1))
-
-print()
-print('=='*20)
-print()
-
-print('o dobro do numero digitado é {}'.format(numero*2))
-print('o triplo do numero digitado é {}'.format(numero*3))
-print('a raiz quadrada do numero digitado é {}'.format(numero**0.5))
---
+++ EXERCICIOS/EXERCICIOS CURSO/ex006.py
@@ -0,0 +1,9 @@
+numero = float(input('digite um numero: '))
+
+print()
+print('=='*20)
+print()
+
+print('o dobro do numero digitado é {}'.format(numero*2))
+print('o triplo do numero digitado é {}'.format(numero*3))
+print('a raiz quadrada do numero digitado é {}'.format(numero**(1/2)))
