854672f25a99a032a0156e5daa8846a532970016
Seu Nome <seuemail@gmail.com>
2026-06-21 10:12:02 -0300

    42

--- EXERCICIOS/ex095.py
+++ EXERCICIOS/ex095.py
@@ -13,32 +13,32 @@ while True:
     time.append(jogador.copy())
     while True:
         resp = str(input('Deseja continuar? [S/N] ')).strip().upper()
-        if resp in 'SN':
+        if resp in 'SN' and resp != '':
             break
-        print('Erro resposta invalida.')
+        print('Erro! Resposta inválida.')
     if resp == 'N':
         break
 print('-=' * 30)
-print('cod', end='')
+print(f'{"cod":<4}', end='')
 for i in jogador.keys():
     print(f'{i:<15}', end='')
 print()
-print('-' * 40)
-for k,v in enumerate(time):
-    print(f'{k:>4}', end='')
-    for d in v.values():
-        print(f'{str(d):>15}', end='')
-    print()
-print('-' * 40)
+print('-' * 50)
+for k, v in enumerate(time):
+    print(f'{k:<4}', end='')
+    print(f'{v["jogador"]:<15}', end='')
+    print(f'{str(v["gols"]):<15}', end='')
+    print(f'{str(v["total"]):<15}')
+print('-' * 50)
 while True:
-    busca = int(input('Mostrar o dado de qual jogador?'))
+    busca = int(input('Mostrar o dado de qual jogador? (999 para parar): '))
     if busca == 999:
         break
-    if busca >= len(time):
-        print('Erro, não tem jogador com esse valor na lista.')
+    if busca >= len(time) or busca < 0:
+        print(f'Erro, não existe jogador com o código {busca} na lista.')
     else:
-        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}:')
-        for i,g in enumerate(time[busca]['gols']):
-            print(f'      No jogo {i+1} fez {g} gols.')
-    print('-'*40)
-print('Volte sempre.')
+        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]["jogador"]}:')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols.')
+    print('-' * 50)
+print('<< Volte sempre! >>')
