e9205e604ee8bb73ded36b0287c0e51c312f8164
Seu Nome <seuemail@gmail.com>
2026-07-07 10:00:25 -0300

    61-EXERCICIO 109 FORMAT

--- EXERCICIOS/ex109.py
+++ EXERCICIOS/ex109.py
@@ -2,7 +2,14 @@ from ex109 import moeda
 
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {moeda.moeda(p)} e {moeda.metade(p)}')
-print(f'O dobro de {moeda.moeda(p)} e {moeda.dobro(p)}')
-print(f'Aumentado 10% temos {moeda.aumentar(p, 10)}')
-print(f'Diminuindo em 10% temos {moeda.diminuir(p, 10)}')
+formato = str(input('Quer formatar o preço? [S/N]: ')).strip().upper()
+if formato == 'S':
+    print(f'A metade de {moeda.moeda(p)} e {moeda.metade(p, True)}')
+    print(f'O dobro de {moeda.moeda(p)} e {moeda.dobro(p, True)}')
+    print(f'Aumentado 10% temos {moeda.aumentar(p, 10, True)}')
+    print(f'Diminuindo em 10% temos {moeda.diminuir(p, 10, True)}')
+else:
+    print(f'A metade de {moeda.moeda(p)} e {moeda.metade(p)}')
+    print(f'O dobro de {moeda.moeda(p)} e {moeda.dobro(p)}')
+    print(f'Aumentado 10% temos {moeda.aumentar(p, 10)}')
+    print(f'Diminuindo em 10% temos {moeda.diminuir(p, 10)}')
