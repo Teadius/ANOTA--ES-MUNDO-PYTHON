8a377fa2816f9a1fc30593874e9c1fa24cd79e1f
Seu Nome <seuemail@gmail.com>
2026-07-15 07:46:08 -0300

    71-aula0 POO

---
+++ AULAS POO/aula0.py
@@ -0,0 +1,43 @@
+# POO (Programação Orientada a Objetos)
+# OOP (Object Oriented Programming)
+# OOAD (Object Oriented Analysis and Design)
+
+# Esse curso tratara sobre a programação orientada a objetos.
+# Começando a historia de on veio.
+# Para que serve.
+# As vantagens do usu.
+# Entendendo o paradigma.
+
+# Fundamentação:
+# Classe.
+# objeto.
+# Atributos.
+# Metodos.
+# Estado.
+# instancia.
+# visibilidade.
+# Os pilares do POO. (Esse curso ira considerar 4 pilares)
+
+# Pilares do POO:
+# 1 - Abstração
+# 2 - Encapsulamento
+# 3 - Herança
+# 4 - Polimorfismo
+
+# Outros conceitos:
+# Getters e Setters.
+# self e super.
+# dunder methods.
+# associação.
+# agregação.
+# compposição.
+
+# Ao fim o curso tratara sobre bancos de dados.
+# Acesso de bases locais.
+# SQLite3.
+#Object-relational mapping. (ORM)
+
+
+
+# OBS: O curso ira ter bibliografia recomenmdada para estudo.
+# E so abrir o python.org e abrir a opção localizada apra PTBR.
